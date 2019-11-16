689cb029b79f268f3973585238cce28e948c90ea
Marcus Ramos <marcusramos@outlook.ie>
2019-11-16 23:25:25 +0000

    Bumping cli version

--- collector/__init__.py
+++ collector/__init__.py
@@ -17,7 +17,7 @@ def define_log_level(level):
 
 __pkg_name__ = "aws-lambda-log-collector"
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 __description__ = "A handy CLI to help you gather Lambda Functions logs"
 
